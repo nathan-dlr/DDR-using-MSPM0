cbe0f13e7f23773767d0b7cfa3a9c6fc22f9c42b
nathan-dlr <nathan.delarosa89@gmail.com>
2024-06-05 19:04:41 -0500

    deleted sounds, update gitignore

--- Debug/SmallFont.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-SmallFont.o: ../SmallFont.cpp \
- /Users/nathan/Desktop/MSPM0_ValvanoWare_V2/ECE319K_Lab9H/SmallFont.h \
- /Users/nathan/Desktop/MSPM0_ValvanoWare_V2/inc/ST7735.h
-
-/Users/nathan/Desktop/MSPM0_ValvanoWare_V2/ECE319K_Lab9H/SmallFont.h:
-
-/Users/nathan/Desktop/MSPM0_ValvanoWare_V2/inc/ST7735.h:
